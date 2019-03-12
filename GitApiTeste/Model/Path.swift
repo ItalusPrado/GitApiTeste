f48f72b19e85a33d01a18520d23ff88b7c60b2f4
Italus Rodrigues <italusprado@acad.ifce.edu.br>
2019-03-12 15:54:58 -0300

    preparando modelo da tela inicial

---
+++ GitApiTeste/Model/Path.swift
@@ -0,0 +1,13 @@
+//
+//  Path.swift
+//  GitApiTeste
+//
+//  Created by Italus Rodrigues do Prado on 12/03/19.
+//  Copyright © 2019 Italus Rodrigues do Prado. All rights reserved.
+//
+
+import Foundation
+
+struct Github{
+    static let listUsers = "https://api.github.com/users"
+}
